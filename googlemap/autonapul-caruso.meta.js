b47743153512796de9629e0906efe6d05ec12a52
Michael Mraka <michael.mraka@redhat.com>
2015-11-09 14:00:39 +0100

    fix to reflect caruso -> zemtu transistion

--- googlemap/autonapul-caruso.meta.js
+++ googlemap/autonapul-caruso.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        Autonapul - Caruso reservation modifications
-// @namespace   https://carusocarsharing.com/
+// @namespace   https://zemtu.com/
 // @updateURL   http://autonapul.org/misc/autonapul-caruso.meta.js
 // @downloadURL http://autonapul.org/misc/autonapul-caruso.user.js
 // @description Autonapul - Caruso reservation modifications
-// @include     https://carusocarsharing.com/admin/main/reservationgui/canvas/*
-// @include     https://autonapul.carusocarsharing.com/admin/main/reservationgui/canvas/*
-// @version     20150413.1
+// @include     https://zemtu.com/reservation/*
+// @include     https://autonapul.zemtu.com/reservation/*
+// @version     20151109.1
 // @author      Michael Mraka <mraka@autonapul.cz>
 // @grant       none
 // ==/UserScript==
